454675e27b0a42283e96b1214e3d9bfa9fa934b4
nostopbutton <pete@nostopbutton.com>
2013-07-17 15:29:14 -0700

    HW 1 Part3

--- hw1/part3.rb
+++ hw1/part3.rb
@@ -1,11 +1,25 @@
 def combine_anagrams(words)
- # YOUR CODE HERE
- sorted_words = []
- words.each {|word| sorted_words.push word.upcase.chars.sort.join}
- return sorted_words
+  # YOUR CODE HERE
+  sorted_words = []
+  words.each { |word| sorted_words.push word.upcase.chars.sort.join }
+  
+  anagram_hash = {}
+  sorted_words.each_with_index { |sorted_word, index|
+  	# we set a hash key to an empty array, but only if it is currently nil.
+    anagram_hash [sorted_word] ||= []
+    # we add the word to the array
+    anagram_hash[sorted_word] << words[index]
+  }
+
+  anagram_groups = []
+
+  anagram_hash.each { |key, group| anagram_groups.push group }
+  
+  return anagram_groups
+
 end
 
 
 test_words = ['cars', 'for', 'potatoes', 'racs', 'four', 'scar', 'creams', 'scream'] 
 
-puts combine_anagrams test_words
+print combine_anagrams test_words
