1dd679ccd92a05c5f7909257ee33ab043688b003
nostopbutton <pete@nostopbutton.com>
2013-07-16 18:47:59 -0700

    Starting HW3

--- hw1/part3.rb
+++ hw1/part3.rb
@@ -1,3 +1,11 @@
 def combine_anagrams(words)
  # YOUR CODE HERE
+ sorted_words = []
+ words.each {|word| sorted_words.push word.upcase.chars.sort.join}
+ return sorted_words
 end
+
+
+test_words = ['cars', 'for', 'potatoes', 'racs', 'four', 'scar', 'creams', 'scream'] 
+
+puts combine_anagrams test_words
