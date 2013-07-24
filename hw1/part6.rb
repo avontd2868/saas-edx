391f1c74f5af5c3e1f2578f9b1f6997265c1b267
nostopbutton <pete@nostopbutton.com>
2013-07-24 02:49:03 -0700

    Parts 6&7

--- hw1/part6.rb
+++ hw1/part6.rb
@@ -24,9 +24,71 @@ puts "10.euros #{10.euros}"
 puts "10.euros.in(:rupees) #{10.euros.in(:rupees)}"
 
 class String
-  # YOUR CODE HERE
+  def palindrome?
+    # First convert string to lowercase and remove all non-word characters
+    clean_str = self.downcase.gsub(/\d|\W/,"")
+
+    # Then compare it with the revese of itself
+    clean_str == clean_str.reverse
+  end
 end
 
+puts "ooo".palindrome?
+
 module Enumerable
-  # YOUR CODE HERE
+  def palindrome?
+  # First convert string to lowercase and remove all non-word characters
+  # clean_str = self.downcase.gsub(/\d|\W/,"")
+
+
+
+
+  # Then compare it with the revese of itself
+  if self.is_a?(::Hash)
+    print "is hash"
+    return false
+  elsif self.respond_to?(:reverse)
+    print "reverse"
+    return self == self.reverse 
+  else 
+    return self.to_a == self.to_a.reverse 
+  end
+  # puts "self.is_a?(Array) #{self.is_a?(Array)}"
+#   if !self.is_a?(::Hash)
+#     puts "array"
+#     self.each_with_index {|x, index| 
+#       puts "each"
+#       idx = index +1;
+#       puts "self.count: #{self.count} idx: #{idx}"
+#       puts "x: #{x} self[self.count - idx]: #{self[self.count - idx]}"
+#       puts x==self[self.count - idx]
+#       if !(x==self[self.count - idx])
+#         return false
+#       end
+#     }
+#   else
+#     puts "else"
+#     return false
+#   end
+
+# puts "about to return"
+#   return true
 end
+
+end
+puts [1,2,2,1].palindrome?
+
+hash = {:a=>1, :b=>2}
+puts hash.palindrome?
+
+class Foo
+  include Enumerable
+  def initialize
+    @palindrome = ["a","m","a"]
+  end
+  def each
+    @palindrome.each {|e| yield e}
+  end
+end
+
+puts "Foo #{Foo.new.palindrome?}"
--- hw1/part7.rb
+++ hw1/part7.rb
@@ -1,4 +1,31 @@
 class CartesianProduct
   include Enumerable
   # YOUR CODE HERE
+  def initialize(a, b)
+    @a = a
+    @b = b
+  end
+
+  attr_accessor :a
+  attr_accessor :b
+
+  def each
+  	ret = []
+  	# puts "a #{a} b #{b}"
+  	a.each {|x| b.each{|y| yield [x,y]}}
+  	# print ret
+  	return ret
+  end
 end
+
+puts "1"
+c = CartesianProduct.new([:a,:b], [4,5])
+c.each { |elt| puts elt.inspect }
+# [:a, 4]
+# [:a, 5]
+# [:b, 4]
+# [:b, 5]
+puts "2"
+c = CartesianProduct.new([:a,:b], [])
+c.each { |elt| puts elt.inspect }
+# Nothing printed since Cartesian product of anything with an empty collection is empty
