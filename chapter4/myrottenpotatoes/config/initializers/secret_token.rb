7c1925d7d4286e02694893bb99485c80fc642267
nostopbutton <pete@nostopbutton.com>
2013-07-27 01:08:15 -0700

    Chapter 4 Rotten Potatoes app

---
+++ chapter4/myrottenpotatoes/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = 'd9f11641acdcf25482d55cb481e69fa07b9226c4990de20025ce7663aa5c4841b6baeff194be363a26e8bd1a4864b43868ec45258f568b3e919448f5672a2cd8'
