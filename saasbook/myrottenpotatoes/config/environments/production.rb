0755e98435e882f326afc14d3f756088f1d8cb63
nostopbutton <pete@nostopbutton.com>
2013-07-27 18:54:03 -0700

    Updated dir structure

--- saasbook/myrottenpotatoes/config/environments/production.rb
+++ saasbook/myrottenpotatoes/config/environments/production.rb
@@ -15,7 +15,8 @@ Myrottenpotatoes::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # git clone https://github.com/saasbook/hw2_rottenpotatoes.git
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
